ec5b755bdf6c9b4602f96ba10bb33e8f91d1a4b7
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-16 09:58:45 -0400

    WIP to add react router

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import JournalForm from './containers/JournalForm';
+import Home from './containers/Home';
 import logo from './logo.svg';
 import './App.css';
+
+const routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="/daily" component={JournalForm} />
+  </Route>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={hashHistory}>
+        {routes}
+      </Router>
     );
   }
 }
