5186fefba87911cef76a65aa42f511d66c4a369d
suzyng83209 <suzyn.one@gmail.com>
2017-09-16 10:45:30 -0400

    import

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import moment from 'moment';
 import './App.css';
 import { db } from './firebase';
+import Typewriter from './components/Typewriter';
 
 const submitClick = () => {
   const rating = 79;
@@ -31,13 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Typewriter phrase="Log(me)." time={200} />
         <button onClick={submitClick}>Update Firebase</button>
       </div>
     );
