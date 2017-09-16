7531423dc25ee361ba008207b6974908196f304d
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-16 10:53:33 -0400

    Switch over to react router 4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
+import { BrowserRouter } from 'react-router-dom'
 import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
