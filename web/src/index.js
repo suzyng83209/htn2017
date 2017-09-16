a5b7914baea17c62ac03c2227702242ae295b2e9
Robert Riachi <robertjr@riachi.com>
2017-09-16 02:32:12 -0400

    Added Firebase

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+function submitClick() {
+
+  var firebaseRef = firebase.database().ref();
+  //Gets rating based off Emoji chosen
+  var rating = 0;
+  //Use day of week library
+  var dayOfWeek = moment().day();
+
+  var tmp = firebaseRef.child("Total");
+
+  //Sets day of the week to rating chosen
+  firebaseRef.child(dayOfWeek).set(rating);
+  firebaseRef.child("Total").set(tmp + rating.toString());
+}
