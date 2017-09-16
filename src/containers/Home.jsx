365f30b4450b6476ebc246edae659936b7118867
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-16 10:56:49 -0400

    Move firebase code into home page
    - preserve App.js as a pure routing/App level component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { db } from '../firebase';
+import moment from 'moment';
+import Typewriter from '../components/Typewriter';
+
+const submitClick = () => {
+  const rating = 79;
+  const today = moment().format('dddd');
+  var temp;
+
+  db
+    .ref()
+    .child(today)
+    .set(rating);
+
+  db
+    .ref()
+    .child('Total')
+    .once('value')
+    .then(({ node_ }) => {
+      db
+        .ref()
+        .child('Total')
+        .set(node_.value_ + rating.toString());
+    });
+};
 
 class Home extends Component {
   render() {
@@ -12,6 +37,10 @@ class Home extends Component {
         <p className="Home-intro">
           This is temporary text, this will eventually be the home page.
         </p>
+        <div className="App">
+         <Typewriter phrase="Log(me)." time={200} />
+         <button onClick={submitClick}>Update Firebase</button>
+        </div>
       </div>
     );
   }
