ca0eaf0c2f2171806f0f8c9b6b68418cb1adaaf2
suzyng83209 <suzyn.one@gmail.com>
2017-09-16 12:06:40 -0400

    adding route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import JournalForm from './containers/JournalForm';
 import Home from './containers/Home';
+import Input from './components/Input';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/daily" component={JournalForm} />
+        <Route path="/input" component={Input} />
       </Switch>
     );
   }
