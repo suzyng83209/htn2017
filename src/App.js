71f1d4b8ccfa8c24a1be70ce585f2c0d522a1663
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-16 12:22:12 -0400

    Add material ui

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import JournalForm from './containers/JournalForm';
 import Home from './containers/Home';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/daily" component={JournalForm} />
-      </Switch>
+      <MuiThemeProvider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/daily" component={JournalForm} />
+        </Switch>
+      </MuiThemeProvider>
     );
   }
 }
