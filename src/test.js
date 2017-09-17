29d2f8af89bf8b9e33ea32d7f0c87b477c9734df
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-17 01:08:05 -0400

    Add text analysis and setWeather functions

--- src/test.js
+++ src/test.js
@@ -4,28 +4,59 @@ import ReactDOM from 'react-dom';
 import { OpenWeatherMap } from 'react-weather';
 import axios from 'axios';
 
+function stringToAscii(s) {
+  var ascii="";
+  if(s.length>0)
+    for(var i=0; i < s.length; i++)
+    {
+      var c = ""+s.charCodeAt(i);
+      while(c.length < 3)
+       c = "0"+c;
+      ascii += c;
+    }
+  return(ascii);
+}
+
 export default class Test extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      moodStr: '',
+      moodStr: '0123456789',
       test: ''
     };
   }
 
+  textAnalysis = () => {
+    return fetch('https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/languages', {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json',
+       'Ocp-Apim-Subscription-Key': '82eaf8d7aaf141e7b51d448766e4325a',
+      },
+      body: ({
+        "text": "I am really sad",
+      }),
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  // moodStr is a string of numbers, of all recorded moods
   checkMood = () => {
-    for (var i = 0, len = this.state.moodStr.length; i < len; i++) {
-      var store = this.state.moodStr.split('');
-      var frequency = {}; // array of frequency.
-      var max = 0; // holds the max frequency.
-      var result; // holds the max frequency element.
-      for (var v in store) {
-        frequency[store[v]] = (frequency[store[v]] || 0) + 1; // increment frequency.
-        if (frequency[store[v]] > max) {
-          // is this frequency > max so far ?
-          max = frequency[store[v]]; // update max.
-          result = store[v]; // update result.
-        }
+    var store = this.state.moodStr.split('').slice(-30);
+    var frequency = {}; // array of frequency.
+    var max = 0; // holds the max frequency.
+    var result; // holds the max frequency element.
+    for (var v in store) {
+      frequency[store[v]] = (frequency[store[v]] || 0) + 1; // increment frequency.
+      if (frequency[store[v]] > max) {
+        // is this frequency > max so far ?
+        max = frequency[store[v]]; // update max.
+        result = store[v]; // update result.
       }
     }
     console.log(result);
@@ -47,15 +78,18 @@ export default class Test extends React.Component {
     });
   };
 
+  setWeather = (weather) => {
+    db.ref().child('Weather').set(weather);
+  }
+
 	getWeather = () => {
 		var key = "http://api.openweathermap.org/data/2.5/weather?q=Waterloo&APPID=002768ee775ba2d1d80d3508fb8a5bc0";
 		return axios.get(key)
 		.then((json) => {
-			var data = JSON.stringify(json);
-			console.log(data.weather[0].description);
-			return data.weather[0].description;
-		}).catch((err) => {
-			console.log('the error for this call', err)
+			const weather = json.data.weather[0].description;
+      return this.setWeather(weather);
+    }).catch((err) => {
+			return err;
 		})
 	}
 
@@ -63,7 +97,7 @@ export default class Test extends React.Component {
     return (
       <div style={{height:"100vh", marginTop:"50%"}}>
         {' '}
-        <button onClick={this.getWeather}> click me to log str </button>
+        <button onClick={this.textAnalysis}> click me to log str </button>
       </div>
     );
   };
