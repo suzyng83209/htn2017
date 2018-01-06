08dc1aa75c6960984e530b88a2493ab62f0e2944
suzyng83209 <suzyn.one@gmail.com>
2018-01-06 16:30:12 -0500

    add /

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'logme-app/build')));
+app.use(express.static(path.join(__dirname, '/logme-app/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'logme-app/build/index.html'));
+    res.sendFile(path.join(__dirname + '/logme-app/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
